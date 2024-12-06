6a1c3ae69d32d390de38af71a90b41ed4460102b
Keshav Prajapati <keshavpj1711@gmail.com>
2024-12-06 19:55:41 +0530

    starting out with basic str of a program in cpp

---
+++ Basics/until_vars/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+// Includes the iostream library, which defines cout, cin, and others
+
+using namespace std; 
+// Makes the std namespace available, so you don't need to write std:: before cout, cin, etc.
+
+int main()
+{
+  cout << "Hello World!!" << endl;
+  return 0;
+}
