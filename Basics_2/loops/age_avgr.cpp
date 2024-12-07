ed1ce576d67fc52215e7f4c1ce32ab0b5d088da2
Keshav Prajapati <keshavpj1711@gmail.com>
2024-12-07 13:33:36 +0530

    removed the redundant code i.e. checking for same thing again and again

--- Basics_2/loops/age_avgr.cpp
+++ Basics_2/loops/age_avgr.cpp
@@ -5,16 +5,16 @@ using namespace std;
 // We want to take the inputs as long as someone enters -1 which is when we terminate our loop
 int main()
 {
-  int age = 0;
+  int age;
   int total = 0;
   float counter = 0;
 
-  while(age>=0) {
+  cin >> age;
+
+  while(age!=-1) {
+    total += age;
+    counter++;
     cin >> age;
-    if (age >= 0) {
-      total += age;
-      counter++;
-    }
   }
 
   cout << "Average age: " << (total/counter) << endl;
